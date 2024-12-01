08a405e9cbfc81493274353cb0b26dd0d740cdfe
Adi.Gurhan <adi.gurhan@outlook.com>
2024-12-01 01:45:52 -0600

    2:10:34

--- api/src/db/productsSchema.ts
+++ api/src/db/productsSchema.ts
@@ -1,9 +1,9 @@
 import { pgTable, serial, text, varchar, doublePrecision } from "drizzle-orm/pg-core";
 
-export const products = pgTable("products", {
+export const productsTable  = pgTable("products", {
   id: serial("id").primaryKey(), // Automatically incrementing primary key
   name: varchar("name", { length: 255 }).notNull(), // Product name (required)
-  description: text(), // Product description (optional)
+  description: text(),
   image: varchar("image", { length: 255 }), // Image URL (optional)
   price: doublePrecision("price").notNull(), // Product price (required)
 });
